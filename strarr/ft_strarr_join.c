aa52fe5860ecf9993f974afdda5654d7b5a14586
adryeeel <adryelrbrz+github@gmail.com>
2024-09-09 00:53:06 +0100

    Initial string array join function

---
+++ strarr/ft_strarr_join.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strarr_join.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 19:13:46 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/08 23:42:55 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_strarr.h"
+
+char **ft_strarr_join(char *strarr_a[], char *strarr_b[])
+{
+	(void)strarr_a;
+	(void)strarr_b;
+	return (NULL);
+}
