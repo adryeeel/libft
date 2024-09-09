3ace782e5f5767c88b908b69484fa9173cb179da
adryeeel <adryelrbrz+github@gmail.com>
2024-09-09 12:52:45 +0100

    Add ft_strarr_push function to append a string to a string array
    The ft_strarr_push function appends a single string to a null-terminated array
    of strings. It dynamically allocates memory for the new array and adds the
    new string at the end, ensuring proper null termination.
    
    This function simplifies the process of extending an array of strings,
    handling memory allocation and array resizing automatically.

--- strarr/ft_strarr_push.c
+++ strarr/ft_strarr_push.c
@@ -6,15 +6,38 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:14:02 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/08 23:49:23 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/09 12:27:48 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_strarr.h"
 
+static void ft_copy(char *dst[], char *src[], size_t len)
+{
+	size_t i;
+
+	i = 0;
+	while (i < len)
+	{
+		dst[i] = ft_strdup(src[i]);
+		i++;
+	}
+}
+
 char **ft_strarr_push(char *strarr[], char *str)
 {
-	(void)strarr;
-	(void)str;
-	return (NULL);
+	size_t len;
+	char **new;
+
+	len = ft_strarr_length(strarr);
+	new = ft_calloc(len + 2, sizeof(char *));
+
+	if (!new)
+		return (NULL);
+
+	ft_copy(new, strarr, len);
+	new[len] = ft_strdup(str);
+	new[len + 1] = NULL;
+
+	return (new);
 }
