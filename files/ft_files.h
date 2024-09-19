b7b9a6d1865e83cd9a4097bb724c239d7fb2ebb6
adryeeel <adryelrbrz+github@gmail.com>
2024-09-19 14:23:30 +0100

    Refactor header file to abstract internal functions

--- files/ft_files.h
+++ files/ft_files.h
@@ -6,27 +6,16 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 00:04:33 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/19 01:29:57 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/19 13:35:32 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_FILES_H
 #define FT_FILES_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 2
-#endif
-
-#ifndef NULL_BYTE
-#define NULL_BYTE 1
-#endif
-
 #include <fcntl.h>
 #include <unistd.h>
 
-#include "../mem/ft_mem.h"
-#include "../string/ft_string.h"
-
-char *ft_readl(int fd);
+char	*ft_readl(int fd);
 
 #endif
