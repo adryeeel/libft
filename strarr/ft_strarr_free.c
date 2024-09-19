52570f4d6302eac2ce17ef0c8fe2a502d90d8ac8
adryeeel <adryelrbrz+github@gmail.com>
2024-09-19 01:00:33 +0100

    Free the string array

--- strarr/ft_strarr_free.c
+++ strarr/ft_strarr_free.c
@@ -6,13 +6,19 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:13:44 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/08 20:37:43 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/19 00:53:27 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_strarr.h"
 
-void ft_strarr_free(char *strarr[])
+void	ft_strarr_free(t_strarr *strarr)
 {
-	(void)strarr;
+	size_t	i;
+
+	i = 0;
+	while (i < strarr->length)
+		free(strarr->data[i++]);
+	free(strarr->data);
+	free(strarr);
 }
