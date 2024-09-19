34ee7745958f29a03d54e2daec51104783e69b38
adryeeel <adryelrbrz+github@gmail.com>
2024-09-19 01:02:19 +0100

    Combine two arrays of strings into a single one

--- strarr/ft_strarr_merge.c
+++ strarr/ft_strarr_merge.c
@@ -6,15 +6,28 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 12:47:32 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/09 12:48:27 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/19 00:53:49 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_strarr.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strarr_merge.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 12:47:32 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/10 02:12:43 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_strarr.h"
 
-static void ft_copy(char *dst[], char *src[], size_t len)
+static void	ft_copy(char *dst[], char *src[], size_t len)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (i < len)
@@ -24,23 +37,21 @@ static void ft_copy(char *dst[], char *src[], size_t len)
 	}
 }
 
-char **ft_strarr_merge(char *strarr_a[], char *strarr_b[])
+t_strarr	*ft_strarr_merge(t_strarr *strarr_a, t_strarr *strarr_b)
 {
-	size_t len_a;
-	size_t len_b;
+	t_strarr	*new;
 
-	char **new;
-
-	len_a = ft_strarr_length(strarr_a);
-	len_b = ft_strarr_length(strarr_b);
-	new = ft_calloc(len_a + len_b + NULL_BYTE, sizeof(char *));
-
+	new = ft_strarr_create();
 	if (!new)
 		return (NULL);
-
-	ft_copy(new, strarr_a, len_a);
-	ft_copy(new + len_a, strarr_b, len_b);
-	new[len_a + len_b] = NULL;
-
+	new->length = strarr_a->length + strarr_b->length;
+	new->data = ft_calloc(new->length + NULL_BYTE, sizeof(char *));
+	if (!new->data)
+	{
+		free(new);
+		return (NULL);
+	}
+	ft_copy(new->data, strarr_a->data, strarr_a->length);
+	ft_copy(new->data + strarr_a->length, strarr_b->data, strarr_b->length);
 	return (new);
 }
