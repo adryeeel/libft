e5cb43dc22e8f7477c30c101f336747adbfb0f6d
adryeeel <adryelrbrz+github@gmail.com>
2024-09-26 01:13:58 +0100

    Return the dequeued element

--- queue/ft_dequeue.c
+++ queue/ft_dequeue.c
@@ -6,22 +6,25 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:00:35 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/26 00:47:33 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/26 01:11:35 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_queue.h"
 
-void	ft_dequeue(t_queue *q)
+void	*ft_dequeue(t_queue *q)
 {
+	void	*data;
 	t_node	*node;
 
 	if (!q->head)
-		return ;
+		return (NULL);
 	node = q->head;
 	q->head = q->head->next;
 	if (!q->head)
 		q->tail = NULL;
 	q->length--;
+	data = node->data;
 	free(node);
+	return(data);
 }
