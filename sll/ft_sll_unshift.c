d224aa6b0037f938413134753b22facc5ca11843
adryeeel <adryelrbrz+github@gmail.com>
2024-09-26 14:41:18 +0100

    Update to handle node allocation

--- sll/ft_sll_unshift.c
+++ sll/ft_sll_unshift.c
@@ -6,28 +6,26 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:40:33 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/13 21:41:51 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/26 14:32:43 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sll.h"
 
-static void	ft_sll_to_premier(t_sll *self, t_sll_node *new_node)
+void ft_sll_unshift(t_sll *self, void *data)
 {
-	new_node->next = NULL;
-	self->head = new_node;
-	self->tail = new_node;
-	self->size++;
-}
+	t_sll_node *new_node;
 
-void	ft_sll_unshift(t_sll *self, t_sll_node *new_node)
-{
-	if (self->size == 0)
-	{
-		ft_sll_to_premier(self, new_node);
-		return ;
-	}
-	self->size++;
+	new_node = (t_sll_node *)malloc(sizeof(t_sll_node));
+	if (!new_node)
+		return;
+	new_node->data = data;
 	new_node->next = self->head;
+	if (self->size++ == 0)
+	{
+		self->head = new_node;
+		self->tail = new_node;
+		return;
+	}
 	self->head = new_node;
 }
