fc3032043bdf97e55eecf48c914a6aeabd32d7fc
adryeeel <adryelrbrz+github@gmail.com>
2024-09-26 14:38:24 +0100

    Initialize a sll struct

--- sll/ft_sll_init.c
+++ sll/ft_sll_init.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sll_create.c                                    :+:      :+:    :+:   */
+/*   ft_sll_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:52:09 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/16 17:50:13 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/26 14:22:02 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sll.h"
 
-t_sll	*ft_sll_create(void)
+void	ft_sll_init(t_sll *self)
 {
-	t_sll	*list;
-
-	list = (t_sll *)malloc(sizeof(t_sll));
-	list->size = 0;
-	list->head = NULL;
-	list->tail = NULL;
-	return (list);
+	self->size = 0;
+	self->head = NULL;
+	self->tail = NULL;
 }
