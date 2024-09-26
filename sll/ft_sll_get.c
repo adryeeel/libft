bceb848ffb7a94cedfb98a9674b8c1b2239287af
adryeeel <adryelrbrz+github@gmail.com>
2024-09-26 14:38:00 +0100

    Change return to list's data instead of node

--- sll/ft_sll_get.c
+++ sll/ft_sll_get.c
@@ -6,13 +6,13 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:46:06 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/14 00:04:05 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/26 14:21:16 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sll.h"
 
-t_sll_node	*ft_sll_get(t_sll *self, size_t index)
+void	*ft_sll_get(t_sll *self, size_t index)
 {
 	t_sll_node	*found_node;
 
@@ -25,5 +25,5 @@ t_sll_node	*ft_sll_get(t_sll *self, size_t index)
 	found_node = self->head->next;
 	while (--index > 0)
 		found_node = found_node->next;
-	return (found_node);
+	return (found_node->data);
 }
