0b23ced48574d938338259fc5a5d3753889774d8
adryeeel <adryelrbrz+github@gmail.com>
2024-09-27 01:26:27 +0100

    Return the remove element

--- sll/ft_sll_pop.c
+++ sll/ft_sll_pop.c
@@ -6,25 +6,31 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:31:46 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/14 00:03:35 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/27 01:23:13 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sll.h"
 
-void	ft_sll_pop(t_sll *self)
+void *ft_sll_pop(t_sll *self)
 {
-	t_sll_node	*new_tail;
+	void *data;
+	t_sll_node *new_tail;
 
-	if (self->size <= 1)
+	if (!self->size)
+		return (NULL);
+	if (self->size-- == 1)
 	{
+		data = self->tail->data;
+		free(self->tail);
 		self->head = NULL;
 		self->tail = NULL;
-		self->size = 0;
-		return ;
+		return (data);
 	}
-	new_tail = ft_sll_get(self, self->size - 2);
+	new_tail = ft_sll_get(*self, self->size - 2);
 	new_tail->next = NULL;
+	data = self->tail->data;
+	free(self->tail);
 	self->tail = new_tail;
-	self->size--;
+	return (data);
 }
