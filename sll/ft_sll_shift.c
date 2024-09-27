9f2d8cd3058d39d2fb12958bf9c898d70bee38ca
adryeeel <adryelrbrz+github@gmail.com>
2024-09-27 01:27:07 +0100

    Return the data from the element

--- sll/ft_sll_shift.c
+++ sll/ft_sll_shift.c
@@ -6,21 +6,28 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:39:08 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/13 21:39:23 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/27 01:22:50 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sll.h"
 
-void	ft_sll_shift(t_sll *self)
+void	*ft_sll_shift(t_sll *self)
 {
-	if (self->size <= 1)
+	void		*data;
+	
+	if (!self->size)
+		return (NULL);
+	if (self->size-- == 1)
 	{
+		data = self->head->data;
+		free(self->head);
 		self->head = NULL;
 		self->tail = NULL;
-		self->size = 0;
-		return ;
+		return (data);
 	}
+	data = self->head->data;
+	free(self->head);
 	self->head = self->head->next;
-	self->size--;
+	return (data);
 }
