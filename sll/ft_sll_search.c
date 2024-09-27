eaee3b6f0a94fe391035c5ca233f19829a673645
adryeeel <adryelrbrz+github@gmail.com>
2024-09-27 01:27:59 +0100

    Add function to search comparasion

--- sll/ft_sll_search.c
+++ sll/ft_sll_search.c
@@ -6,20 +6,20 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:19:18 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/26 14:20:05 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/27 00:04:31 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_sll.h"
 
-void *ft_sll_search(t_sll *self, void *data)
+void *ft_sll_search(t_sll self, void *target, int (*f)(void *, void *))
 {
 	t_sll_node	*current_node;
 
-	current_node = self->head;
+	current_node = self.head;
 	while (current_node != NULL)
 	{
-		if (current_node->data == data)
+		if (f(current_node->data, target))
 			return (current_node->data);
 		current_node = current_node->next;
 	}
