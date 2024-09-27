abf1472a59528236ef1be41eb74430e579f04cd8
adryeeel <adryelrbrz+github@gmail.com>
2024-09-27 01:28:12 +0100

    Change functions signatures

--- sll/ft_sll.h
+++ sll/ft_sll.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 21:20:59 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/26 14:36:22 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/27 01:24:45 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,14 @@ typedef struct s_sll
 }						t_sll;
 
 void					ft_sll_init(t_sll *self);
-void					ft_sll_destroy(t_sll *self);
-void					ft_sll_swap(t_sll *self, int x, int y);
-void					*ft_sll_get(t_sll *self, size_t index);
-void					*ft_sll_search(t_sll *self, void *data);
+void					ft_sll_destroy(t_sll self);
+void					ft_sll_swap(t_sll *self, int x, int y); 
+void					*ft_sll_get(t_sll self, size_t index);
+void					*ft_sll_search(t_sll self, void *target, int (*f)(void*, void*));
 void					ft_sll_delete(t_sll *self, size_t index);
-void					ft_sll_map(t_sll *list, void (*f)(void *));
-
-void					ft_sll_pop(t_sll *self);
-void					ft_sll_shift(t_sll *self);
+void					ft_sll_map(t_sll self, void (*f)(void *));
+void					*ft_sll_pop(t_sll *self);
+void					*ft_sll_shift(t_sll *self);
 void					ft_sll_push(t_sll *self, void *data);
 void					ft_sll_unshift(t_sll *self, void *data);
 
