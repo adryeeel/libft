40779abe05f65a34f425984c113d085091691691
adryeeel <adryelrbrz+github@gmail.com>
2024-11-07 23:40:56 +0000

    Add condition to check null string

--- string/ft_strlen.c
+++ string/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:14:04 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/13 22:23:40 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/11/07 23:40:21 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
+	if (!s)
+		return (0);
 	len = 0;
-	while (s[len++])
-		;
-	return (--len);
+	while (s[len])
+		len++;
+	return (len);
 }
